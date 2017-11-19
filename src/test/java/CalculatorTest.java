aff4410465f1156f0b00f50fc025250dfa59d781
rudenko174 <rudenko.qa@gmail.com>
2017-11-20 01:55:45 +0200

    test for Calculator.exe

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -1,82 +1,58 @@
 import org.junit.*;
-import java.io.File;
+import java.net.MalformedURLException;
+import java.net.URL;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 
 public class CalculatorTest {
 
     private static WebDriver driver;
+    private static WebDriverWait wait;
+
 
     @BeforeClass
-    public static void start()
-    {
+    public static void start() throws MalformedURLException {
         //ChromeDriverManager.getInstance().setup();
-
-        System.setProperty("webdriver.chrome.driver", "C:\\Driver\\chromedriver.exe");
-        driver = new ChromeDriver();
-
+        DesiredCapabilities cap = new DesiredCapabilities();
+        cap.setCapability("app", "D:\\Calculator.exe");
+        driver = new RemoteWebDriver(new URL("http://localhost:9999"), cap);
+        wait = new WebDriverWait(driver, 35);
     }
 
     @Before
     public void cleanup()
     {
-        ClassLoader classLoader = getClass().getClassLoader();
-        File file = new File(classLoader.getResource("calc.html").getFile());
-        driver.get("file:///" + file.getAbsolutePath());
 
         //driver.get("file:///C:/Users/Dima/Desktop/Training_center/Auto/calc.html");
 
-        driver.findElement(By.xpath("//input[@value='C']")).click();
+        //driver.findElement(By.name("C")).click();
     }
 
 
     @Test
     public void add_test()
     {
-        driver.findElement(By.xpath("//input[@value='5']")).click();
-        driver.findElement(By.xpath("//input[@value='+']")).click();
-        driver.findElement(By.xpath("//input[@value='4']")).click();
-        driver.findElement(By.xpath("//input[@value='=']")).click();
-        Assert.assertEquals("9", driver.findElement(By.id("resultsbox")).getAttribute("value"));
+        wait.until(ExpectedConditions.presenceOfAllElementsLocatedBy(By.id("1")));
+        driver.findElement(By.name("5")).click();
+        driver.findElement(By.name("5")).click();
+        driver.findElement(By.name("5")).click();
+        driver.findElement(By.name("+")).click();
+        driver.findElement(By.name("5")).click();
+        driver.findElement(By.name("=")).click();
+        Assert.assertEquals("560", driver.findElement(By.id("1000")).getText());
     }
 
-
-    @Test
-    public void substract_test()
-    {
-    driver.findElement(By.xpath("//input[@value='7']")).click();
-    driver.findElement(By.xpath("//input[@value='-']")).click();
-    driver.findElement(By.xpath("//input[@value='1']")).click();
-    driver.findElement(By.xpath("//input[@value='=']")).click();
-    Assert.assertEquals("6", driver.findElement(By.id("resultsbox")).getAttribute("value"));
-    }
-
-    @Test
-    public void divide_test()
-    {
-        driver.findElement(By.xpath("//input[@value='8']")).click();
-        driver.findElement(By.xpath("//input[@value='/']")).click();
-        driver.findElement(By.xpath("//input[@value='2']")).click();
-        driver.findElement(By.xpath("//input[@value='=']")).click();
-        Assert.assertEquals("4", driver.findElement(By.id("resultsbox")).getAttribute("value"));
-    }
-
-
-    @Test
-    public void multiply_test()
-    {
-        driver.findElement(By.xpath("//input[@value='7']")).click();
-        driver.findElement(By.xpath("//input[@value='x']")).click();
-        driver.findElement(By.xpath("//input[@value='3']")).click();
-        driver.findElement(By.xpath("//input[@value='=']")).click();
-        Assert.assertEquals("21", driver.findElement(By.id("resultsbox")).getAttribute("value"));
-    }
 
     @After
     public void stop()
     {
+        //driver.quit();
     }
 
 }
